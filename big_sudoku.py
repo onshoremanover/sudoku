8365cc3cd59f588b0b28aaddd5996297dfd8762e
Lukas Pellegrini <lukas.a.pellegrini@gmail.com>
2022-04-15 17:15:19 +0200

    biggy sudoku created

--- big_sudoku.py
+++ big_sudoku.py
@@ -11,9 +11,15 @@ nr_of_row = 9
 nr_of_depth = 10
 
 
-init_matrix = np.array([[[3., 0., 0],
-                        [0., 1., 0.],
-                        [0., 0., 2.]]])
+init_matrix = np.array([[[3., 0., 0.,    9., 4., 0.,    0., 0. ,1.],
+                        [ 0., 1., 0.,    0., 4., 6.,    0., 4., 2.],
+                        [ 0., 0., 0.,    0., 0., 0.,    0., 0., 0.],
+                        [ 0., 0., 0.,    0., 0., 0.,    0., 0., 0.],
+                        [ 0., 0., 0.,    0., 0., 0.,    0., 0., 3.],
+                        [ 0., 0., 0.,    0., 0., 0.,    0., 0., 2.],
+                        [ 0., 0., 0.,    0., 0., 4.,    0., 0., 2.],
+                        [ 0., 0., 0.,    0., 0., 3.,    0., 0., 2.],
+                        [ 0., 0., 0.,    0., 0., 5.,    6., 7., 8.]]])
 
 def create_3d_matrix(depth, row, col):
     """Fills the backtracking matrix with zeros"""
@@ -26,17 +32,17 @@ def fill_3d_matrix(matrix, depth, row, col, value):
 
 def put_depth_in_matrix(matrix, depth, row, col, value):
     """Populates the backtracking matrix with increasing numbers as to fill it for checking them later"""
-    for i in range(0,3):
-        for j in range(0,3):
-            for k in range(0,3):
+    for i in range(0,9):
+        for j in range(0,9):
+            for k in range(0,9):
                 matrix[depth+i][row+j][col+k]=value+i
     return matrix    
 
 def check_if_matrix_is_full(matrix):
     """Check for the beginning if everything the matrix is full"""
-    for i in range(1,4):
-        for j in range(0,3):
-            for k in range(0,3):
+    for i in range(1,10):
+        for j in range(0,9):
+            for k in range(0,9):
                 if matrix[i][j][k]==0:
                     return False
     return True
@@ -48,18 +54,18 @@ def append_matrix_to_list(sudoku, matrix):
 
 def check_if_matrix_is_full_and_append(sudoku, matrix):
     """Check for the beginning if everything the matrix is full"""
-    for i in range(1,3):
-        for j in range(0,2):
-            for k in range(0,2):
+    for i in range(1,9):
+        for j in range(0,8):
+            for k in range(0,8):
                 if matrix[i][j][k]==0:
                     return False
     return True
 
 def remove_duplicates(sudoku):
     """Removes the duplicates from the backtracking matrix"""
-    for i in range(1,4):
-        for j in range(0,3):
-            for k in range(0,3):
+    for i in range(1,10):
+        for j in range(0,9):
+            for k in range(0,9):
                 if sudoku[0][j][k]!=0:
                         sudoku[i][j][k]=0    
     return sudoku
@@ -68,7 +74,7 @@ def clear_row(sudoku, row, col):
     """Clears the row"""
     value = int(sudoku[0][row][col])
     print("value von row", value)
-    for i in range(0,3):
+    for i in range(0,9):
         print(i,row,col)
         sudoku[value][row][i]=0
     return sudoku
@@ -77,7 +83,7 @@ def clear_col(sudoku, row, col):
     """Clears the cols"""
     value = int(sudoku[0][row][col])
     print("value= ", value)
-    for i in range(0,3):
+    for i in range(0,9):
         sudoku[value][i][col]=0
     return sudoku
 
@@ -92,7 +98,7 @@ def clear_col(sudoku, row, col):
 
 
 def main():
-    matrix=create_3d_matrix(depth=3, row=3, col=3)
+    matrix=create_3d_matrix(depth=9, row=9, col=9)
 
     matrix=put_depth_in_matrix(matrix=matrix, depth=0, row=0, col=0, value=1)
     matrix1=append_matrix_to_list(sudoku=init_matrix, matrix=matrix)
